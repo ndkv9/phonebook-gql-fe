e5044e353ccd3e00ebd3381f704ad0b66b7c6b65
ndkv9 <vu.nguyen101@gmail.com>
2021-03-10 11:54:22 +0200

    add Persons component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { gql, useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+
+const ALL_PERSONS = gql`
+	query {
+		allPersons {
+			name
+			phone
+			id
+		}
+	}
+`
+
 const App = () => {
+	const result = useQuery(ALL_PERSONS)
+
+	if (result.loading) {
+		return <div>loading...</div>
+	}
+
 	return (
 		<div>
-			<h3>hello koba</h3>
+			<Persons persons={result.data.allPersons} />
 		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -3,29 +3,21 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+import {
+	ApolloClient,
+	ApolloProvider,
+	HttpLink,
+	InMemoryCache,
+} from '@apollo/client'
 
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	link: new HttpLink({ uri: 'http://localhost:4000' }),
 })
 
-const query = gql`
-	query {
-		allPersons {
-			name
-			phone
-			address {
-				street
-				city
-			}
-			id
-		}
-	}
-`
-
-client.query({ query }).then(response => {
-	console.log(response.data)
-})
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
