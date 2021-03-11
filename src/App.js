859c40dc61b951ac20dca9a506e976f82a40b681
ndkv9 <vu.nguyen101@gmail.com>
2021-03-11 11:32:39 +0200

    add Notify component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import { useQuery } from '@apollo/client'
 import { useError } from './hooks/index'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import Notify from './components/Notify'
 import { ALL_PERSONS } from './queries'
-
-const Notify = ({ errorMessage }) => {
-	if (!errorMessage) {
-		return null
-	}
-	return <div style={{ color: 'red' }}>{errorMessage}</div>
-}
 
 const App = () => {
 	const result = useQuery(ALL_PERSONS)
