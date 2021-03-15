8d8bab5523933fd6d480b1320755f998063374da
ndkv9 <vu.nguyen101@gmail.com>
2021-03-15 20:16:17 +0200

    implement user login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
+
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 import { ALL_PERSONS } from './queries'
 
 const Notify = ({ errorMessage }) => {
@@ -17,9 +19,16 @@ const App = () => {
 	const [token, setToken] = useState(null)
 	const [errorMessage, setErrorMessage] = useState(null)
 	const result = useQuery(ALL_PERSONS)
+	const client = useApolloClient()
 
 	if (result.loading) {
 		return <div>loading...</div>
+	}
+
+	const logout = () => {
+		setToken(null)
+		localStorage.clear()
+		client.resetStore()
 	}
 
 	const notify = message => {
@@ -40,6 +49,7 @@ const App = () => {
 	}
 	return (
 		<div>
+			<button onClick={logout}>logout</button>
 			<Notify errorMessage={errorMessage} />
 			<Persons persons={result.data.allPersons} />
 			<PersonForm setError={notify} />
---
+++ src/components/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
+
+const LoginForm = ({ setError, setToken }) => {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	const [login, result] = useMutation(LOGIN, {
+		onError: error => {
+			setError(error.graphQLErrors[0].message)
+		},
+	})
+
+	// use useEffect for localStorage
+	useEffect(() => {
+		if (result.data) {
+			const token = result.data.login.value
+			setToken(token)
+
+			localStorage.setItem('phonenumbers-user-token', token)
+		}
+	}, [result.data])
+
+	const submit = async event => {
+		event.preventDefault()
+		login({ variables: { username, password } })
+	}
+
+	const handleUsernameChange = ({ target }) => {
+		setUsername(target.value)
+	}
+
+	const handlePasswordChange = ({ target }) => {
+		setPassword(target.value)
+	}
+
+	return (
+		<div>
+			<h2>form</h2>
+			<form onSubmit={submit}>
+				<div>
+					username{' '}
+					<input type='text' value={username} onChange={handleUsernameChange} />
+				</div>
+				<div>
+					password{' '}
+					<input
+						type='password'
+						value={password}
+						onChange={handlePasswordChange}
+					/>
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	)
+}
+
+export default LoginForm
